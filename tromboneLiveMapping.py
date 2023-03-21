dc7e93bd2735a13963a98c67b15cd92dd6e11842
Tristan_Mesureau <tristan.mesureau@gmail.com>
2023-03-21 20:07:38 +0100

    Switch formatting of time with formatting of position because time will need mouse positions

--- tromboneLiveMapping.py
+++ tromboneLiveMapping.py
@@ -54,20 +54,6 @@ mapBPM = 185
 rec = Recorder(mode='6') 
 rec.startThreads() # /!\ blocking instruction, press escape to finish the recording
 
-# Convert time from milliseconds to bar : if using osu file, hitobjects format is "x,y,time,type,hitSound,objectParams,hitSample"
-timeMSToBar = lambda timeInMS : ((timeInMS/1000)*mapBPM/60) # timeS = (bar*4)*60/bpm
-# mapTimings = [52637, 52761, 52886, 53011, 53135, 53260, 53384, 53726, 53850, 53975, 54100, 54474, 54848, 54972, 55097, 55222, 55595, 55969, 56343, 56467, 56592, 56717, 57091, 57465, 57839, 57963, 58088, 58213, 58586, 58710, 58835, 58960, 59084, 59209, 59333, 60113, 60299, 60486, 60674, 60861, 61048, 61235, 61422, 61608, 61794, 61981, 62169, 62356, 62543, 62730, 62917, 63104, 63290, 63477, 63665, 63852, 64039, 64226, 64413, 64600, 64724, 64849, 64974, 65098, 65223, 65347, 66095, 66281, 66468, 66656, 66843, 67030, 67217, 67404, 67590, 67776, 67963, 68151, 68338, 68525, 68712, 68899, 69086, 69272, 69459, 69647, 69834, 70021, 70208, 70581, 70705, 70830, 70955, 71079, 71204, 71328, 72076, 72262, 72449, 72637, 72824, 73011, 73198, 73385, 73571, 73757, 73944, 74132, 74319, 74506, 74693, 74880, 75067, 75253, 75440, 75628, 75815, 76002, 76189, 76376, 76562, 76686, 76811, 76936, 77060, 77185, 77309, 78057, 78243, 78430, 78618, 78805, 78992, 79179, 79366, 79552, 79738, 79925, 80113, 80300, 80487, 80674, 80861, 81048, 81234, 81421, 81609, 81796, 81983, 82170, 82544, 82668, 82793, 82918, 83042, 83167, 83291, 84038, 84162, 84288, 84412, 84786, 84911, 85036, 85160, 85533, 85658, 85782, 85907, 86281, 86405, 86530, 86655, 86779, 86904, 87028, 87153, 87279, 87403, 87777, 87902, 88027, 88151, 88524, 88649, 88773, 88898, 89272, 89396, 89521, 89646, 89770, 89895, 90019] # regex : "^\d+,\d+,(\d+),.*$\n" and keep group 1
-timeToBar = lambda time : (time*mapBPM/60) # timeS = (bar*4)*60/bpm
-mapTimings = rec.seq.timeFromStart
-mapTimingsInBars = list(map(timeToBar, mapTimings))
-# Make sure that we start song at zero + one bar for example :
-barOffset = mapTimingsInBars[0] - 4
-mapTimingsInBars = [b - barOffset for b in mapTimingsInBars]
-mapTimingsInBars = [round(e,2) for e in mapTimingsInBars]
-mapTimingsInBarsWithDuration = []
-for i in range(round(len(mapTimingsInBars)/2)):
-    mapTimingsInBarsWithDuration.append((mapTimingsInBars[2*i], mapTimingsInBars[2*i+1])) # TODO : this trick can also work for simple sliders, but if the slider oscillates we must break it into multiple sliders, but while holding the key, so the big change will be to record mouse move as well to detect extremums
-
 # Convert Y axis to MIDI pitch : if using osu file, we probably need to convert from normalized space to Full HD resolution : "equivalent to a pixel when osu! is running in 640x480 resolution" => actually, max limits in x,y = (512,384) + invert y axis
 # yNormalized = [96, 103, 129, 157, 129, 103, 96, 76, 154, 200, 257, 76, 76, 76, 76, 76, 76, 76, 76, 76, 76, 76, 74, 74, 74, 74, 74, 74, 96, 104, 123, 157, 124, 102, 93, 256, 153, 72, 153, 72, 256, 153, 72, 256, 153, 72, 153, 72, 256, 153, 72, 256, 153, 72, 153, 72, 256, 153, 72, 96, 103, 129, 157, 129, 103, 96, 256, 153, 72, 153, 72, 256, 153, 72, 256, 153, 72, 153, 72, 256, 153, 72, 256, 153, 72, 153, 72, 256, 153, 96, 104, 123, 157, 124, 102, 93, 257, 152, 73, 159, 72, 256, 153, 72, 258, 153, 74, 155, 77, 256, 156, 75, 255, 153, 72, 154, 259, 258, 153, 72, 87, 96, 124, 157, 122, 93, 76, 256, 153, 72, 153, 72, 256, 153, 72, 256, 153, 72, 153, 72, 256, 153, 72, 256, 153, 72, 153, 72, 256, 153, 96, 104, 123, 157, 124, 102, 93, 80, 92, 124, 95, 92, 124, 154, 124, 124, 154, 167, 154, 87, 96, 124, 157, 122, 93, 76, 94, 125, 95, 92, 124, 154, 124, 124, 154, 167, 154, 87, 96, 124, 157, 122, 93, 76] # regex : "^\d+,(\d+),\d+,.*$\n" and keep group 1
 # fullHDCoordinates = [lerpAxis(y,0,384,125,125+841) for y in yNormalized]
@@ -80,6 +66,21 @@ pitchList = [round(e,2) for e in pitchList]
 scalePitchList = getPitchForScale("major", "C")
 scalePitchList = periodizeSerie(scalePitchList, 1) # spread it on the other octave (higher)
 quantizedPitchStartList = pitchList #[quantizeNote(n, scalePitchList) for n in pitchList]
+
+# Convert time from milliseconds to bar : if using osu file, hitobjects format is "x,y,time,type,hitSound,objectParams,hitSample"
+timeMSToBar = lambda timeInMS : ((timeInMS/1000)*mapBPM/60) # timeS = (bar*4)*60/bpm
+# mapTimings = [52637, 52761, 52886, 53011, 53135, 53260, 53384, 53726, 53850, 53975, 54100, 54474, 54848, 54972, 55097, 55222, 55595, 55969, 56343, 56467, 56592, 56717, 57091, 57465, 57839, 57963, 58088, 58213, 58586, 58710, 58835, 58960, 59084, 59209, 59333, 60113, 60299, 60486, 60674, 60861, 61048, 61235, 61422, 61608, 61794, 61981, 62169, 62356, 62543, 62730, 62917, 63104, 63290, 63477, 63665, 63852, 64039, 64226, 64413, 64600, 64724, 64849, 64974, 65098, 65223, 65347, 66095, 66281, 66468, 66656, 66843, 67030, 67217, 67404, 67590, 67776, 67963, 68151, 68338, 68525, 68712, 68899, 69086, 69272, 69459, 69647, 69834, 70021, 70208, 70581, 70705, 70830, 70955, 71079, 71204, 71328, 72076, 72262, 72449, 72637, 72824, 73011, 73198, 73385, 73571, 73757, 73944, 74132, 74319, 74506, 74693, 74880, 75067, 75253, 75440, 75628, 75815, 76002, 76189, 76376, 76562, 76686, 76811, 76936, 77060, 77185, 77309, 78057, 78243, 78430, 78618, 78805, 78992, 79179, 79366, 79552, 79738, 79925, 80113, 80300, 80487, 80674, 80861, 81048, 81234, 81421, 81609, 81796, 81983, 82170, 82544, 82668, 82793, 82918, 83042, 83167, 83291, 84038, 84162, 84288, 84412, 84786, 84911, 85036, 85160, 85533, 85658, 85782, 85907, 86281, 86405, 86530, 86655, 86779, 86904, 87028, 87153, 87279, 87403, 87777, 87902, 88027, 88151, 88524, 88649, 88773, 88898, 89272, 89396, 89521, 89646, 89770, 89895, 90019] # regex : "^\d+,\d+,(\d+),.*$\n" and keep group 1
+timeToBar = lambda time : (time*mapBPM/60) # timeS = (bar*4)*60/bpm
+mapTimings = rec.seq.timeFromStart
+mapTimingsInBars = list(map(timeToBar, mapTimings))
+# Make sure that we start song at zero + one bar for example :
+barOffset = mapTimingsInBars[0] - 4
+mapTimingsInBars = [b - barOffset for b in mapTimingsInBars]
+mapTimingsInBars = [round(e,2) for e in mapTimingsInBars]
+mapTimingsInBarsWithDuration = []
+# for i in range(round(len(mapTimingsInBars)/2)):
+#     mapTimingsInBarsWithDuration.append((mapTimingsInBars[2*i], mapTimingsInBars[2*i+1])) # TODO : this trick can also work for simple sliders, but if the slider oscillates we must break it into multiple sliders, but while holding the key, so the big change will be to record mouse move as well to detect extremums
+fullHDCoordinates
 
 # Generate the Trombone champ file :
 author = "Peter Lambert"
